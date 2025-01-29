86b51f8589f4f04a99b3a2f47d889d3c10c45134
chuhezhe <chuhezhe0807@163.com>
2025-01-29 21:53:22 +0800

    Spring cloud; · @Value("${}")获取配置+@RefreshScope 实现自动刷新;

--- services/service-order/src/main/java/com/chuhezhe/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/chuhezhe/order/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.chuhezhe.order.controller;
 import com.chuhezhe.order.bean.Order;
 import com.chuhezhe.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @Create 2025/1/29 12:34
  * @Version 1.0
  */
+@RefreshScope // 当配置修改时，自动刷新 orderTimeout orderAutoConfirm 两个值
 @RestController
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @Value("${order.timeout}")
+    String orderTimeout;
+
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    // 获取发布的配置
+    @GetMapping("/config")
+    public String config() {
+        return "order.timeout=" + orderTimeout + "; order.auto-confirm=" + orderAutoConfirm;
+    }
 
     // 创建订单
     @GetMapping("/create")
